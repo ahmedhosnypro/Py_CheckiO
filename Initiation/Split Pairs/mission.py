3f7d58ad0d7236b7de6880c7cd62fb9a95812372
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-26 10:14:35 +0300

    Initial commit

---
+++ Initiation/Split Pairs/mission.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+
+def split_pairs(text: str) -> Iterable[str]:
+    # your code here
+    return []
+
+
+print("Example:")
+print(list(split_pairs("abcd")))
+
+assert list(split_pairs("abcd")) == ["ab", "cd"]
+assert list(split_pairs("abc")) == ["ab", "c_"]
+assert list(split_pairs("abcdf")) == ["ab", "cd", "f_"]
+assert list(split_pairs("a")) == ["a_"]
+assert list(split_pairs("")) == []
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
